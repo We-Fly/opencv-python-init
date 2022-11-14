72b994ea0277d7ee46f846bec7ecc53240cefab8
discodyer <cody23333@gmail.com>
2022-11-14 22:00:06 +0800

    修改图片大小

--- demo.py
+++ demo.py
@@ -3,5 +3,6 @@ import sys
 img = cv.imread(cv.samples.findFile("logo.png"))
 if img is None:
     sys.exit("Could not read the image.")
+img = cv.resize(img, (300, 300))
 cv.imshow("We-Fly", img)
 k = cv.waitKey(0)
